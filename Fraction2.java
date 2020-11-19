90e4c1bcb767fff7d70049585412a333c1cdbf65
Skywalker409 <71032363+Skywalker409@users.noreply.github.com>
2020-11-18 19:01:51 -0500

    Add files via upload

--- Fraction2.java
+++ Fraction2.java
@@ -3,28 +3,34 @@ public class Fraction2 {
 // we tried to do the fraction project with another way but that was poo so i made a whole new one and called it Fraction2 pls 
 	//don't take points off
 	//Partner = paul.epic
-
+/*add me on xbox 
+ * my gamertag: skywalker499
+ * well play MC and MW if you have it. if you dont want to thats fine paul and i were talking about
+ * you liking games so... if you wanna play
+ * 
+ */
 
 		
 		private int num = 1;
 		private int dem = 2;
+		private int Num2 =3;
+		private int Dem2 =2;
 		
-		public static double posdiff(double a, double b) {
-			return ((a/b)-(a/b));
-			
+		public static double posdiff(double num, double num2) {
+			return (num - num2);	
 		}
-		
-		 public static double add(double a, double b) {
-				return ((a + a)/ b);
-		 
+		 public static double add(double num, double num2) {
+				return ((num + num2));
 		 }
-		 public static double multi(double a, double b) {
-			 b = b*b;
-			 
-				return (b);
+		 public static String multi(double num, double dem, double num2, double dem2) {
+			 int dem3 = (int) (dem*dem2);
+			 int num3 = (int)(num*num2);
+			 String multians = (num3 + "/"+ dem3);
+				return (multians);
 		 }
-		 public static double div(double a, double b) {
-				return ((a/b)/ (a/b));
+		 public static String div(double num, double dem, double num2, double dem2) {
+			String Div = (int)(num*num2)+ "/"+ (int)(num2*dem2);	
+						return (Div);
 		 }
 		 public  double change() {
 			
@@ -33,22 +39,19 @@ public class Fraction2 {
 			System.out.println(ansdiff);
 			return ansdiff;
 		 }
-		 public   String ans(double num2, double dem1)
+		 public   String ans(int num, int dem, int num2, int dem2)
 		 {
 				
 				Fraction2 poo = new Fraction2();
-				double  sub = poo.posdiff(num, dem);
-				System.out.println( "Subtraction: " + (int)sub + "/" + (int)sub);
-				double add = poo.add(num, dem);
-				System.out.println("Addition:"+(int)add+"/1");
-				double mult = poo.multi(num, dem);
-				System.out.println("Multiplication:" + num + "/"+(int)mult);
-				double div = poo.div(num, dem);
-				System.out.println("Divition:"+(int)div + "/1");
-				return " ";
-			 
-			 
+				System.out.println("The Fractions used are:(" + num+ "/"+dem+ ") and (" +num2+"/"+dem2+")");
+				double  sub = poo.posdiff(num, num2);
+				System.out.println( "Subtraction: "+ (int)sub + "/" + (int)dem);
+				double add = poo.add(num, num2);
+				System.out.println("Addition:"+ (int)add+"/"+ dem);
+				String mult = poo.multi(num, dem, num2, dem2);
+				System.out.println("Multiplication:" + mult);
+				String div = poo.div(num, dem, dem2, num2);
+				System.out.println("Division:"+ div );
+				return " "; 
 		 }
 		}
-		
-
--- Fraction2test.java
+++ Fraction2test.java
@@ -1,13 +1,21 @@
-
+import java.util.Scanner;
 public class Fraction2test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner poo = new Scanner(System.in);
 		
-		Fraction2 poo = new Fraction2();
-		int num = 1;
-		int dem = 2;
-		String print = poo.ans(num, dem);
+		System.out.println("what is the numerator of the first Fraction?");
+		System.out.println("*the denomiatos will be the same");
+			int num = poo.nextInt();
+			System.out.println("what is the denominator of the first one?");
+		int dem = poo.nextInt();
+		System.out.println("What is the Numerator of the second Fraction");
+		int num2= poo.nextInt();
+		int dem2 = dem;
+
+		Fraction2 pood = new Fraction2();
+				String print = pood.ans(num, dem, num2, dem2);
 		System.out.println(print);
 		
 	}
